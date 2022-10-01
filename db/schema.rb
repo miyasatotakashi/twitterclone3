0d6733aed242aca23381bc24d078c82508eca161
takashi miyasato <on.the.line.mt215@gmail.com>
2022-10-02 00:47:55 +0900

    schema.rbをマイグラーションしました

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2022_10_01_154037) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
